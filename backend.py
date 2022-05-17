4bac8f32ad07db3c5056d48c3ae018da84726f1c
Delcoma <saikal.satybaldievaaa@gmail.com>
2022-05-17 19:34:02 +0600

    added gitignore

---
+++ backend.py
@@ -0,0 +1,36 @@
+"""Github --> GIT-system,GIT(service)
+git - system is free open source version control system, for trtacking changes in source code, 
+has brain tree(древовидную)structure.
+github - web based hosting service for version control.
+"""
+# Repository - container with ur code
+# commit - screenshot опреленного (этапа кода)состояния. какая-то версия,уровень , 
+# можно предлагать комменты. we can turnback to them whenever we want
+
+#main branch - хранит initial state 
+#____ _____ _____ ______
+#                     _____ _____ _____
+
+
+#pull request - asking someone to merge the branches
+#merge-слияние, adding two branches 
+
+#____ _____ _____ ______
+#git --version --> shows version of ur git
+#git init--> initializes a git repository
+#git status --> shows curent state of our project
+#git add(filename) or . --> adds changes to git 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
